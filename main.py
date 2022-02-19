d7fea94027541151b444e7331ebdb31aabfdeda1
sowbhagyavallich <100018055+sowbhagyavallich@users.noreply.github.com>
2022-02-19 14:23:00 +0530

    Add files via upload

---
+++ main.py
@@ -0,0 +1,31 @@
+#pip install pydrive
+#import Googleauth and Googledrive modules from pydrive
+
+from pydrive.auth import GoogleAuth
+from pydrive.drive import GoogleDrive
+gauth = GoogleAuth()
+drive = GoogleDrive(gauth)
+
+# Download single file using file_id from GoogleDrive
+#
+# file = drive.CreateFile({'id' : '<enter the file_id here>'})
+#
+# mention the file format like jpeg for images, .pdf for pdf files etc
+#
+# file.GetContentFile('filename.pdf')
+
+id = input('enter the folder_id here: ')
+
+#list out the files in the folder
+
+FileList = drive.ListFile({'q': f"'{id}' in parents"}).GetList()
+
+#Downloading the files from a folder
+for file in FileList:
+    print(file['title'])
+    file.GetContentFile(file['title'])
+
+
+
+
+
